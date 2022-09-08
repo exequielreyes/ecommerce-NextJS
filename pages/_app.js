7a9715451876c067492b4e8e38131c17b2f93079
Misael Cortes <misael.cortes39@gmail.com>
2022-09-07 23:15:35 -0300

    Instalacion next-auth para login

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
-import StateWrapper from '../components/stateWrapper'
-import '../styles/globals.css'
+import StateWrapper from "../components/stateWrapper";
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps }) {
-  return(
-    <StateWrapper>
-      <Component {...pageProps} />
-    </StateWrapper>
-  )  
-  
-      
+  return (
+    <SessionProvider>
+      <StateWrapper>
+        <Component {...pageProps} />
+      </StateWrapper>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,45 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from "next/link"
-import Layout from '../components/layout'
-import { getLatestItems } from '../services/itemService'
-import Product from '../components/product'
-import styleProduct from "../styles/product.module.css"
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import Layout from "../components/layout";
+import { getLatestItems } from "../services/itemService";
+import Product from "../components/product";
+import styleProduct from "../styles/product.module.css";
 
-export default function Home({items}) {
+export default function Home({ items }) {
   return (
-    <Layout> 
+    <Layout>
       <div className={styles.banner}>
-
-        <div className={styles.bannerBackground}>  
-        
-        <div className={styles.bannerInfo}>
-          <h2>Shop the Summer 2022 Collection</h2>
-          <p>
-          Level up your comfort this season with our new Winter Collection
-                — all new joggers, beanies, drinkware, and for the first time
-                ever, Octocookie cutters!
-          </p>
-        </div>
-        
+        <div className={styles.bannerBackground}>
+          <div className={styles.bannerInfo}>
+            <h2>Shop the Summer 2022 Collection</h2>
+            <p>
+              Level up your comfort this season with our new Winter Collection —
+              all new joggers, beanies, drinkware, and for the first time ever,
+              Octocookie cutters!
+            </p>
+          </div>
         </div>
       </div>
 
       <h3>Latest Products</h3>
-        <div className={styleProduct.items}>
-          {items && items.map((item) => (
-              <Product key={item.id} item={item} showAs="item" />
-            ))}
-        </div>
-
+      <div className={styleProduct.items}>
+        {items &&
+          items.map((item) => (
+            <Product key={item.id} item={item} showAs="item" />
+          ))}
+      </div>
     </Layout>
-      
-  )
+  );
 }
-
 
 export async function getStaticProps() {
   const res = await getLatestItems();
 
-  return{
+  return {
     props: {
       items: res,
-    }
-  }
+    },
+  };
 }
